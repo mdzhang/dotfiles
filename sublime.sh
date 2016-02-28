88eb299a7ec8541fb7622b25343ae19d1cb1b195
Michelle D Zhang <zhang.michelle.d@gmail.com>
2016-02-28 10:59:12 -0500

    Sublime setup should work properly now

--- sublime.sh
+++ sublime.sh
@@ -1,9 +1,11 @@
 if brew cask list -1 | grep -q "sublime-text3"; then
     cp ./init/sublime/Packages/User/* ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
-    # curl http://sublime.wbond.net/Package%20Control.sublime-package > ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package\ Control.sublime-package
-    # cp ./init/sublime/Installed\ Packages/Package\ Control.sublime-package ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/
+    curl http://sublime.wbond.net/Package%20Control.sublime-package > ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package\ Control.sublime-package
     killall "Sublime Text" &> /dev/null
     subl
 else
     echo "Please use brew to install Sublime Text 3 first"
 fi
+
+# next time you open sublime, packages should be installed, after which you'll have to restart
+# you can update the theme at that point as well, after which you'll need to restart again :/
