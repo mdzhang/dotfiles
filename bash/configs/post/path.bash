01fb3f3f69e21850ce1941432ba7727180185cd1
Michelle D Zhang <zhang.michelle.d@gmail.com>
2017-03-20 09:24:58 -0400

    Assorted

--- bash/configs/post/path.bash
+++ bash/configs/post/path.bash
@@ -6,10 +6,6 @@ if which nodenv > /dev/null; then
   eval "$(nodenv init -)";
 fi
 
-if which docker-machine > /dev/null; then
-  eval "$(docker-machine env)"
-fi
-
 if which direnv > /dev/null; then
   eval "$(direnv hook bash)"
 fi
