1980d6db9f6d475bd393124a79178444a47c4112
Michelle D Zhang <zhang.michelle.d@gmail.com>
2017-07-22 15:13:53 -0400

    brew things

--- configs/bash/configs/brew.bash
+++ configs/bash/configs/brew.bash
@@ -1,3 +1,3 @@
 if [ -f /usr/local/bin/brew ]; then
-  export HOMEBREW_CASK_OPTS="--appdir=/Applications --caskroom=/opt/homebrew-cask/Caskroom"
+  export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 fi
