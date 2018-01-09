f455fedef77a80cade852719fde9abfc1108d533
Michelle D Zhang <zhang.michelle.d@gmail.com>
2018-01-08 21:52:20 -0500

    Additional XDG configs

--- config/sh/usr/xdg/exports.sh
+++ config/sh/usr/xdg/exports.sh
@@ -15,3 +15,8 @@ export INPUTRC="$XDG_CONFIG_HOME"/readline/inputrc
 export LESSHISTFILE="$XDG_CACHE_HOME"/less/history
 
 export HTTPIE_CONFIG_DIR="$XDG_CONFIG_HOME"/httpie
+
+export PYENV_ROOT="$XDG_CONFIG_HOME"/pyenv
+export NODENV_ROOT="$XDG_CONFIG_HOME"/nodenv
+export GOENV_ROOT="$XDG_CONFIG_HOME"/goenv
+export GLIDE_HOME="$XDG_CONFIG_HOME"/glide
