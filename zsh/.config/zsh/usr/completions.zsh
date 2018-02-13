92688ae8bbb02a4bf9a9e768196f69b4ef0d3128
Michelle D Zhang <zhang.michelle.d@gmail.com>
2018-02-13 16:54:53 -0500

    [macos] Fixes after gnu stow move

--- zsh/.config/zsh/usr/completions.zsh
+++ zsh/.config/zsh/usr/completions.zsh
@@ -5,10 +5,3 @@ fi
 if [ $commands[helm] ]; then
   source <(helm completion zsh)
 fi
-
-if [ "$(uname)" = "Darwin" ]; then
-  if which gcloud > /dev/null; then
-    source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
-    source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
-  fi
-fi
