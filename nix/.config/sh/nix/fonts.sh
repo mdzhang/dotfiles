d9ccaa0914c57c9f713dd432257e17dddb56b09e
Michelle D Zhang <zhang.michelle.d@gmail.com>
2018-06-09 23:38:04 -0400

    [konsole] glyph fix; [vim] devicon support
    Install Hack w/ Nerd font patch from
    https://github.com/ryanoasis/nerd-fonts#option-3-install-script
    _Don't_ use awesome-terminal-fonts - the codepoints are totally out of
    line with vim-devicons

--- nix/.config/sh/nix/fonts.sh
+++
@@ -1,8 +0,0 @@
-# Install glyphs by installing https://github.com/gabrielelana/awesome-terminal-fonts
-# Then, b/c codepoint mappings will be different and custom zsh prompts will be borked, follow
-# https://github.com/robbyrussell/oh-my-zsh/issues/1906#issuecomment-275733922
-
-# Install Hack w/ Nerd font patch from https://github.com/ryanoasis/nerd-fonts#option-3-install-script
-export POWERLEVEL9K_MODE='awesome-fontconfig'
-source $HOME/.fonts/*.sh
-
