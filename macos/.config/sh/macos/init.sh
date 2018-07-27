2006abe140769518f26d0d1c4528313c0a911df2
Michelle D Zhang <zhang.michelle.d@gmail.com>
2018-07-27 13:29:07 -0400

    [macos] Homebrew env vars, git ignores

--- macos/.config/sh/macos/init.sh
+++ macos/.config/sh/macos/init.sh
@@ -14,6 +14,23 @@ brew_update() {
   brew cask cleanup
 }
 
+# Bash completion dir: /usr/local/etc/bash_completion.d
+# zsh completions dir: /usr/local/share/zsh/site-functions
+
+if [ -d /usr/local/opt/gnu-sed/bin ]; then
+  export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
+  export MANPATH="/usr/local/opt/gnu-sed/libexec/gnuman:$MANPATH"
+fi
+
+if [ -d /usr/local/opt/coreutils/bin ]; then
+  export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+  export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+fi
+
+if [ -d /usr/local/opt/ncurses/bin ]; then
+  export PATH="/usr/local/opt/ncurses/bin:$PATH"
+fi
+
 if which pyenv-virtualenv > /dev/null; then
   export PYENV_VIRTUALENV_DISABLE_PROMPT=1
   eval "$(pyenv virtualenv-init -)";
