780563041c2919c8e803e2b9cb579730f9a5bf28
Michelle D Zhang <zhang.michelle.d@gmail.com>
2018-08-05 15:20:07 -0400

    [sh] xdg env var for kaggle api token

--- sh/.config/sh/xdg/exports.sh
+++ sh/.config/sh/xdg/exports.sh
@@ -3,3 +3,4 @@
 export LESSHISTFILE="$XDG_DATA_HOME/less/history"
 export HTTPIE_CONFIG_DIR="$XDG_CONFIG_HOME/httpie"
 export _Z_DATA="$XDG_DATA_HOME/z"
+export KAGGLE_CONFIG_DIR="$XDG_CONFIG_HOME/kaggle"
