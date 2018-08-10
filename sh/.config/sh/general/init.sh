058f5cb02a1aa5281fd5d9a206624380c60b1d96
Michelle D Zhang <zhang.michelle.d@gmail.com>
2018-08-10 09:29:09 -0400

    [perf] remove slow startup eval call

--- sh/.config/sh/general/init.sh
+++ sh/.config/sh/general/init.sh
@@ -1,3 +1,4 @@
+# mdzhang: This takes about 150ms :(
 if which anyenv > /dev/null; then
   eval "$(anyenv init - --no-rehash)";
 fi
@@ -13,10 +14,6 @@ fi
 
 if which direnv > /dev/null; then
   eval "$(direnv hook $SHELL_NAME)"
-fi
-
-if which thefuck > /dev/null; then
-  eval "$(thefuck --alias)";
 fi
 
 [ -f ~/.fzf.$SHELL_NAME ] && source ~/.fzf.$SHELL_NAME
