008e8ad475906cca9f0b6a0538d19ef55274b0ba
Michelle D Zhang <zhang.michelle.d@gmail.com>
2018-08-12 11:33:25 -0400

    [fish] more niceties, change z_data dir to be fi/z/sh compat

--- sh/.config/sh/xdg/exports.sh
+++ sh/.config/sh/xdg/exports.sh
@@ -2,5 +2,5 @@
 
 export LESSHISTFILE="$XDG_DATA_HOME/less/history"
 export HTTPIE_CONFIG_DIR="$XDG_CONFIG_HOME/httpie"
-export _Z_DATA="$XDG_DATA_HOME/z"
+export _Z_DATA="$XDG_DATA_HOME/z/data"
 export KAGGLE_CONFIG_DIR="$XDG_CONFIG_HOME/kaggle"
