74267ee84f49c5be146875d4d34ca86a81668cf0
Michelle D Zhang <zhang.michelle.d@gmail.com>
2018-09-08 09:24:19 -0400

    [kitty] some basic configs

--- sh/.config/sh/xdg/exports.sh
+++ sh/.config/sh/xdg/exports.sh
@@ -4,3 +4,5 @@ export LESSHISTFILE="$XDG_DATA_HOME/less/history"
 export HTTPIE_CONFIG_DIR="$XDG_CONFIG_HOME/httpie"
 export _Z_DATA="$XDG_DATA_HOME/z/data"
 export KAGGLE_CONFIG_DIR="$XDG_CONFIG_HOME/kaggle"
+export KITTY_CONFIG_DIRECTORY="$XDG_CONFIG_HOME/kitty"
+
