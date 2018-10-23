56c03f7f747fa19abcdac4db7301071891bb6915
Michelle D Zhang <zhang.michelle.d@gmail.com>
2018-10-23 16:46:38 +0200

    [ruby] ignore warnings

--- ruby/.config/sh/ruby/exports.sh
+++ ruby/.config/sh/ruby/exports.sh
@@ -1,5 +1,7 @@
 export GEM_HOME="$XDG_DATA_HOME/gem"
 export GEM_SPEC_CACHE="$XDG_CACHE_HOME/gem"
+# ignore warnings
+export RUBYOPT="-W0"
 
 # for global ruby packages, else use rbenv
 export PATH="$HOME/.local/share/gem/bin:$PATH"
