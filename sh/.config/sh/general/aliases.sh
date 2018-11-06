f8104e4626fccccc1d416350ba1a1f6ba41ef654
Michelle D Zhang <zhang.michelle.d@gmail.com>
2018-11-06 12:30:50 -0500

    Terraform things

--- sh/.config/sh/general/aliases.sh
+++ sh/.config/sh/general/aliases.sh
@@ -26,3 +26,5 @@ alias la='ll -Alh'         # Lists human readable sizes, hidden files.
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
+
+alias tf='terraform'
