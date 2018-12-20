455c01b670bd714b91c1e34e1f1c820f8990a476
Michelle D Zhang <zhang.michelle.d@gmail.com>
2018-12-20 08:05:48 -0500

    Fixes

--- golang/.config/sh/golang/aliases.sh
+++ golang/.config/sh/golang/aliases.sh
@@ -1,4 +1,4 @@
-# use pb for ultimate plumber instead of up
+# use for ultimate plumber instead of up
 # which collides w/ sh/scripts/up.sh for traversing
 # directories
-alias pb='$GOPATH/bin/up'
+alias ulp='up'
