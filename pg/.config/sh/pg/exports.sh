aa87f01f6bcc75ba082d857c76036ad7777a7f93
Michelle D Zhang <zhang.michelle.d@gmail.com>
2021-04-01 14:56:13 -0500

    misc

--- pg/.config/sh/pg/exports.sh
+++ pg/.config/sh/pg/exports.sh
@@ -1,4 +1,4 @@
 export PSQLRC="$XDG_CONFIG_HOME/pg/psqlrc"
 export PSQL_HISTORY="$XDG_DATA_HOME/pg/psql_history"
 export PGPASSFILE="$XDG_CONFIG_HOME/pg/pgpass"
-export PGSERVICEFILE="$XDG_CONFIG_HOME/pg/pg_service.conf"
+# export PGSERVICEFILE="$XDG_CONFIG_HOME/pg/pg_service.conf"
