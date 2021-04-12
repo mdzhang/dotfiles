6f2063bdba4ebee2777a54db16f60ca6d131ccf2
Michelle D Zhang <mzhang@better.com>
2021-04-12 09:28:57 -0500

    golang, gcloud

--- golang/.config/sh/golang/exports.sh
+++ golang/.config/sh/golang/exports.sh
@@ -1,7 +1,6 @@
 export GOPATH="$HOME/src/go"
-export PATH="$PATH:$GOPATH/bin"
-
-export GLIDE_HOME="$XDG_DATA_HOME/glide"
+export GOBIN="$GOPATH/bin"
+export PATH="$PATH:$GOBIN"
 
 if which asdf > /dev/null; then
   goversion=$(asdf current golang | awk -F ' ' '{print $2}' | tr -d '\n')
--- macos/.config/sh/macos/init.sh
+++ macos/.config/sh/macos/init.sh
@@ -37,6 +37,7 @@ do
   export CFLAGS="-I/usr/local/opt/$pkg/include $CFLAGS"
   export LDFLAGS="-L/usr/local/opt/$pkg/lib $LDFLAGS"
   export CPPFLAGS="-I/usr/local/opt/$pkg/include $CPPFLAGS"
+  # export PKG_CONFIG_PATH="/usr/local/opt/$pkg/lib/pkgconfig"
   export PATH="/usr/local/opt/$pkg/bin:$PATH"
 done
 
@@ -47,3 +48,10 @@ export CFLAGS="-I$(xcrun --show-sdk-path)/usr/include $CFLAGS"
 # NB: do this _after_ homebrew packages added to path, since gnu-bin replaces
 #     a few commands and makes them more linux-y, which grc wants
 [ -f "/usr/local/etc/grc.$SHELL_NAME" ] && . "/usr/local/etc/grc.$SHELL_NAME"
+
+export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+
+gcloud_path="$HOMEBREW_PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.$SHELL_NAME.inc"
+if [ -f "/$gcloud_path" ]; then
+   ."$gcloud_path"
+fi
