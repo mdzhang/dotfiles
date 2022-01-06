a7f14dc7c5fdba6bc9e39b8804ae1279c2772a46
m d zhang <md.zhang.aka@gmail.com>
2022-01-06 12:54:54 -0600

    [go] goroot asdf

--- golang/.config/sh/golang/exports.sh
+++ golang/.config/sh/golang/exports.sh
@@ -3,6 +3,6 @@ export GOBIN="$GOPATH/bin"
 export PATH="$PATH:$GOBIN"
 
 if which asdf > /dev/null; then
-  goversion=$(asdf current golang | awk -F ' ' '{print $2}' | tr -d '\n')
-  export GOROOT="$HOME/.asdf/installs/golang/$goversion/go"
+  GOV=$(asdf where golang)
+  export GOROOT=$GOV/go
 fi
