971b79e112efba90471993f6d155f8b47ba6de0a
meesh <meesh@found.com>
2023-12-14 13:39:59 -0500

    [neovim]: ftype

---
+++ nvim/.config/nvim/lua/config/filetype.lua
@@ -0,0 +1,27 @@
+vim.filetype.add({
+  filename = {
+    [".Rprofile"] = "r",
+    [".arcconfig"] = "json",
+    [".arclint"] = "json",
+    [".dockerignore"] = "conf",
+    [".envrc"] = "sh",
+    [".eslintrc"] = "json",
+    [".esprintrc"] = "json",
+    [".prettierrc"] = "json",
+    [".yamllint"] = "yaml",
+    ["Brewfile"] = "ruby",
+    ["go.mod"] = "go",
+  },
+  pattern = {
+    ["*.cf"] = "dosini",
+    ["*.cfg"] = "dosini",
+    ["*.conf"] = "dosini",
+    ["*.es6"] = "javascript",
+    ["*.jenkinsfile"] = "groovy",
+    ["*.muttrc"] = "muttrc",
+    ["*.soql"] = "sql",
+    ["*.tpl"] = "gohtmltmpl",
+    ["*Dangerfile"] = "ruby",
+    ["*_test.rb"] = "rspec",
+  },
+})
